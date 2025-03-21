59e67155a8ef2551ddda958f056d329dc0c55887
maedachaeva <parkchaeyeon.b@gmail.com>
2025-03-21 15:44:12 +0900

    Refactor: main.js 수정

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 // 절주온 스크립트 파일
-<<<<<<< HEAD
 src = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/js/all.min.js';
-=======
 
 // 헤더
 // 햄버거 버튼 토글
 function toggleFullscreenNav() {
-  var nav = document.getElementById("fullscreenNav");
-  if (nav.style.display === "block") {
-    nav.style.display = "none";
-  } else {
-    nav.style.display = "block";
-  }
+    var nav = document.getElementById('fullscreenNav');
+    if (nav.style.display === 'block') {
+        nav.style.display = 'none';
+    } else {
+        nav.style.display = 'block';
+    }
 }
->>>>>>> fdf0a72c10e98e99fe94a169fb7e16161b237c23
