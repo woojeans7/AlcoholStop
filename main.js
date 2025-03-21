fdf0a72c10e98e99fe94a169fb7e16161b237c23
Yujung Choi <yujung5548@naver.com>
2025-03-21 15:10:29 +0900

    Feat: header 하단 페이지

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 // 절주온 스크립트 파일
+
+// 헤더
+// 햄버거 버튼 토글
+function toggleFullscreenNav() {
+  var nav = document.getElementById("fullscreenNav");
+  if (nav.style.display === "block") {
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "block";
+  }
+}
